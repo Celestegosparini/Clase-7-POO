27a05fb9f6639f6e9d0d8b9ec514ba05737edbb5
Celeste <celegos3@gmail.com>
2018-03-06 08:24:52 -0300

    sttter and getter agregando

--- Cliente-Banco/src/ClienteBanco/Cliente.java
+++ Cliente-Banco/src/ClienteBanco/Cliente.java
@@ -4,5 +4,34 @@ public class Cliente {
 
     private String nombre;
     private String apellido;
-    private int edad;
+     private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+ 
+    @Override
+    public String toString(){
+        return nombre + apellido + edad;
+    }
 }
--- Cliente-Banco/src/ClienteBanco/CuentaCorriente.java
+++ Cliente-Banco/src/ClienteBanco/CuentaCorriente.java
@@ -4,4 +4,33 @@ package ClienteBanco;
 
 public class CuentaCorriente {
     
+    private Cliente tutilar;
+     private int nroCenta;
+    private double saldo;
+
+    public Cliente getTutilar() {
+        return tutilar;
+    }
+
+    public void setTutilar(Cliente tutilar) {
+        this.tutilar = tutilar;
+    }
+
+    public int getNroCenta() {
+        return nroCenta;
+    }
+
+    public void setNroCenta(int nroCenta) {
+        this.nroCenta = nroCenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+   
+    
 }
