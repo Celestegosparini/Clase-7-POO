b5292482fcaf614b8538cd22bd710d888f4f6ee9
Celeste <celegos3@gmail.com>
2018-03-06 09:08:15 -0300

    main

--- Cliente-Banco/src/ClienteBanco/ClienteBanco.java
+++ Cliente-Banco/src/ClienteBanco/ClienteBanco.java
@@ -3,7 +3,13 @@ package ClienteBanco;
 public class ClienteBanco {
 
     public static void main(String[] args) {
+        Cliente aliem = new Cliente();
+        aliem.setNombre("ET");
+        aliem.setApellido("ET");
+        aliem.setEdad(200);
+        
+        
+    }
 
     }
 
-}
